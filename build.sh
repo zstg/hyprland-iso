b991599c80b54585d5bb50a7d8f6e3b4af770551
ZeStig <69384921+zstg@users.noreply.github.com>
2025-08-17 17:17:34 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ sudo rm -rf output.bak
 sudo mv -f output output.bak
 echo "Initializing ISO"
 sudo mkarchiso -v \
+     -D stratos \
+     -L "STRATOS" \
+     -P "StratOS ISO Team" \
      -w output \
      -o output \
      ./
