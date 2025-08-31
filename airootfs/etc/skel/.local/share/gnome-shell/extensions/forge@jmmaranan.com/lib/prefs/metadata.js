4beffc9a843040ef72cbe89915100873bd6b54f6
zstg <o0vckutt@duck.com>
2025-08-31 21:12:32 +0530

    Remove unnecessary config files, incorporate package configs

--- airootfs/etc/skel/.local/share/gnome-shell/extensions/forge@jmmaranan.com/lib/prefs/metadata.js
+++
@@ -1,2 +0,0 @@
-export const developers = Object.entries([
-].reduce((acc, x) => ({ ...acc, [x.email]: acc[x.email] ?? x.name }), {})).map(([email, name]) => name + ' <' + email + '>')
--- airootfs/etc/skel/.local/share/gnome-shell/extensions/space-bar@luchrioh/utils/Widget.js
+++
@@ -1,9 +0,0 @@
-import { Subject } from './Subject.js';
-export function onDestroyed(widget) {
-    const subject = new Subject(void 0);
-    widget.connect('destroy', () => {
-        subject.next();
-        subject.complete();
-    });
-    return subject;
-}
